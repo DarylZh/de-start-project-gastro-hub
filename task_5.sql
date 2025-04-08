ac7fc1e908405efab1375fbb17de70dff6793844
DarylZh <daryl@list.ru>
2025-04-08 22:44:37 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,35 @@
 /*добавьте сюда запрос для решения задания 5*/
+--Задание 5
+WITH menu_cte AS (
+    SELECT
+        r.cafe_name,
+        'Пицца' AS dish_type,
+        p.pizza_name,
+        (p.price::numeric) AS price
+    FROM
+        cafe.restaurants r,
+        jsonb_each_text(r.menu) AS p(pizza_name, price)
+    WHERE
+        r.type = 'pizzeria'
+),
+menu_with_rank AS (
+    SELECT
+        cafe_name,
+        dish_type,
+        pizza_name,
+        price,
+        ROW_NUMBER() OVER (PARTITION BY cafe_name ORDER BY price DESC) AS rank
+    FROM
+        menu_cte
+)
+SELECT
+    cafe_name,
+    dish_type,
+    pizza_name,
+    price
+FROM
+    menu_with_rank
+WHERE
+    rank = 1
+ORDER BY
+    cafe_name;
